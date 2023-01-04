48c641e2b803717a1c1a0160c4948dc35e1fa647
Soerful <andriy.ishchukk@gmail.com>
2023-01-04 14:53:21 +0200

    first commit

---
+++ src/store/cart-context.js
@@ -0,0 +1,11 @@
+import react, { createContext } from "react";
+import React from "react";
+
+const CartContext = React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {},
+    removeItem: (id) => {}
+});
+
+export default CartContext;
